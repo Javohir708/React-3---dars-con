dd4611842308e7dcd468b0edff838e812e9ca1af
Javohir708 <joxauzking@gamil.com>
2025-01-14 18:35:44 +0500

    tayyor

--- src/components/course/Course.jsx
+++ src/components/course/Course.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { USERS } from '../../static'
 
 const Course = () => {
   return (
-    <section>
+    <section className='mb-[120px]'>
         <div className='container mx-auto max-w-[1262px] dod'>
             <div className='flex items-center justify-between mb-[60px]'>
             <div>
@@ -10,12 +11,25 @@ const Course = () => {
                 <h2 className='text-[#1E212C] font-black text-[46px]'>Featured Courses</h2>
             </div>
             <div>
-                <button className='w-[203px] h-[52px] text-[#FF3F3A] border-[1px] border-[#FF3F3A] text-base font-bold'>View all courses</button>
+                <button className='w-[203px] h-[52px] text-[#FF3F3A] border-[1px] border-[#FF3F3A] text-base font-bold rounded'>View all courses</button>
             </div>
             </div>
 
-            <div>
-                
+            <div className='grid grid-cols-2 gap-[32px]'>
+                {
+                    USERS.map((user) => {
+                      return <div key={user.id} className='flex items-center gap-3 border-[1px] border-[solid] rounded'>
+                            <img src={user.url} alt="image" />
+
+                            <div className='p-4'>
+                                <h4 className='p-[1px] rounded text-[#fff] text-sm font-normal mb-[21px] inline-block' style={{background: user.profession.bg}}>{user.profession.title}</h4>
+                                <h2 className='text-[#1E212C] mb-5 text-xl font-bold'>{user.title}</h2>
+                                <strong className='text-[#FF4242] text-lg font-bold'>${user.price}</strong>
+                                <strong className='text[#787A80] text-lg font-normal'>{user.name}</strong>
+                            </div>
+                        </div>
+                    })
+                }
             </div>
         </div>
     </section>
--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,9 @@
 import ads from '../assets/course.svg'
+import managment from '../assets/managment.svg'
+import hr from '../assets/HR.svg'
+import marketinggirl from '../assets/girlmarketing.svg'
+import managmentgirl from '../assets/girlmanag.svg'
+import design from '../assets/design.svg'
 
 export const LINKS = ['About Us', 'Courses', 'Events', 'Blog', 'Contacts']
 
@@ -14,13 +19,68 @@ export const CREATEX = [
 export const USERS = [
     {
         id: 1,
-        url: {ads},
+        url: ads,
         profession: {
             title: "Marketing",
-            bg: "dodgerblue"
+            bg: "#03CEA4"
         },
         title: "The Ultimate Google Ads Training Course",
         price: 100,
-        name: "by Jerome Bell"
+        name: " | by Jerome Bell"
+    },
+    {
+        id: 2,
+        url: managment,
+        profession: {
+            title: "Management",
+            bg: "#5A87FC"
+        },
+        title: "Prduct Management Fundamentals",
+        price: 480,
+        name: " | by Marvin McKinney"
+    },
+    {
+        id: 3,
+        url: hr,
+        profession: {
+            title: "HR & Recruting",
+            bg: "#F89828"
+        },
+        title: "HR  Management and Analytics",
+        price: 200,
+        name: " | by Leslie Alexander Li"
+    },
+    {
+        id: 4,
+        url: marketinggirl,
+        profession: {
+            title: "Marketing",
+            bg: "#03CEA4"
+        },
+        title: "Brand Management & PR Communications",
+        price: 530,
+        name: " | by Kristin Watson"
+    },
+    {
+        id: 5,
+        url: managmentgirl,
+        profession: {
+            title: "Management",
+            bg: "#5A87FC"
+        },
+        title: "Business Development Management",
+        price: 400,
+        name: " | by Dianne Russell"
+    },
+    {
+        id: 6,
+        url: design,
+        profession: {
+            title: "Design",
+            bg: "#F52F6E"
+        },
+        title: "Graphic Design Basic",
+        price: 500,
+        name: " | by Guy Hawkins"
     }
 ]
