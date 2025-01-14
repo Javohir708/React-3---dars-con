b1d8eec8013f7f7b36b5b0e07b834e1b7dad80b6
Javohir708 <joxauzking@gamil.com>
2025-01-14 14:08:36 +0500

    header, hero qismlari tayyor

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+import person from '../../assets/Person.svg'
+import { LINKS } from '../../static'
+
+const Header = () => {
+  return (
+    <header className='pt-5 bg-[#FEDAD4]'>
+        <div className='container mx-auto max-w-[1262px]'>
+            <nav className='flex items-center justify-between h-[52px]'>
+                <div className='flex gap-14'>
+                    <img src={logo} alt="logo image" />
+                <ul className='flex gap-10 text-base font-bold text-[#424551]'>
+                  {
+                    LINKS.map((item, inx) => {
+                      return <li key={inx}>
+                        <a href="#"><span>{item}</span></a>
+                      </li>
+                    })
+                  }
+                </ul>
+                </div>
+                <div className='flex gap-9 items-center'>
+                  <button className='w-64 h-[48px] bg-[#F75E05] text-[#fff] font-bold text-base rounded-[4px]'>Get consultation</button>
+                  <strong className='cursor-pointer'><img className='inline-block' src={person} alt="" /> Log in / Register</strong>
+                </div>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import play from '../../assets/hover.svg'
+import illustration from '../../assets/illustration.svg'
+import dot from '../../assets/dot.svg'
+
+const Hero = () => {
+  return (
+    <section className='pt-32 bg-[#FEDAD4]'>
+        <div className='container mx-auto max-w-[1262px]'>
+            <div className='flex gap-[180px]'>
+                <div className='w-[600px]'>
+                    <h4 className='text-sm font-semibold'><img className='inline-block mb-6 translate-y-2' src={play} alt="Play image" /> Play showreel</h4>
+                    <h2 className='text-[64px] font-black mb-[60px]'>Enjoy studying with Createx Online Courses</h2>
+                    <div className='flex gap-6'>
+                        <button className='w-[148px] h-[52px] border-solid border-2 border-[#F75E05] text-[#FF3F3A] rounded'>About us</button>
+                        <button className='w-[201px] h-[52px] border-solid border-2 bg-[#F75E05] text-[#fff] rounded'>Explore courses</button>
+                    </div>
+                </div>
+
+                <div className='w-3/6 mb-[189px]'>
+                    <img src={illustration} alt="image" />
+                </div>
+            </div>
+
+            <div className='flex gap-[66px] pb-20'>
+                <h2 className='text-[46px] font-black text-[#1E212C]'>1200 <strong className='inline-block text-base font-normal -translate-y-[10px]'>Students graduated</strong></h2>
+                <img src={dot} alt="image" />
+                <h2 className='text-[46px] font-black text-[#1E212C]'>84 <strong className='inline-block text-base font-normal -translate-y-[10px]'>Completed courses</strong></h2>
+                <img src={dot} alt="image" />
+                <h2 className='text-[46px] font-black text-[#1E212C]'>16 <strong className='inline-block text-base font-normal -translate-y-[10px]'>Qualified tutors</strong></h2>
+                <img src={dot} alt="image" />
+                <h2 className='text-[46px] font-black text-[#1E212C]'>5 <strong className='inline-block text-base font-normal -translate-y-[10px]'>Years of experience</strong></h2>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
